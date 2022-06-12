2535f15822a03c38dc8b111186204a9104e5adf8
Conrad Trost <ct.conradtrost@gmail.com>
2022-06-11 21:57:08 -0500

    rename library 'board' to 'tf-board'

--- libs/towards-freedom/tf-board/jest.config.ts
+++ libs/towards-freedom/tf-board/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'towards-freedom-board',
+  displayName: 'tf-board',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/towards-freedom/board',
+  coverageDirectory: '../../../coverage/libs/towards-freedom/tf-board',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
